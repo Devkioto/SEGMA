ced1d236558497ffd05e4cfccd7e5ad752327ff7
Hamza <hamzaelhassnaoui0611@gmail.com>
2025-04-03 10:58:35 +0000

    add logo to navbare

--- segma_front/src/components/Header.jsx
+++ segma_front/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes, faGlobe, faSearch, faShoppingCart, faUser, faLanguage } from "@fortawesome/free-solid-svg-icons";
 import { motion, AnimatePresence } from "framer-motion"; // Import motion and AnimatePresence
 import "../components/Header.css";
+import segma_logo from "../assets/images/segma_logo.png";
 
 function Header(props) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -13,12 +14,12 @@ function Header(props) {
 
   return (
     <>
-      <div id='header' className="col-span-8 h-15 bg-[#FFFFFF] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] flex justify-between items-center">
+      <div id='header' className="col-span-8  h-15 bg-[#FFFFFF] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] flex justify-between items-center">
         {/*//? sidebar & language */}
         <div id="sidebar" className="flex items-center gap-4">
         <motion.button
   id="sidebar-button"
-  className="text-[#C40D2E] text-xl"
+  className="text-[#C40D2E] text-xl cursor-pointer"
   onClick={toggleSidebar}
   initial={{ opacity: 0 }} // Start with opacity 0
   animate={{ opacity: 1 }} // Animate to opacity 1
@@ -39,31 +40,31 @@ function Header(props) {
   </motion.div>
 </motion.button>
 
-          <button className="text-[#C40D2E] text-xl"><FontAwesomeIcon icon={faLanguage} /></button>
+          <button className="text-[#C40D2E] text-xl cursor-pointer"><FontAwesomeIcon icon={faLanguage} /></button>
         </div>
 
-        <ul id="nav" className="hidden md:flex gap-6 text-[#C40D2E] text-sm font-semibold">
+        <ul id="nav" className="hidden items-center md:flex gap-6 text-[#C40D2E] text-sm font-semibold">
           <li><a href="#" className="hover:underline"><p>About Us</p></a></li>
           <li><a href="#" className="hover:underline"><p>Catalogue</p></a></li>
-          <li><p><img src="logo.png" alt="Logo" /></p></li>
+          <li><p href="#" className="cursor-pointer"><img className="h-8" src={segma_logo} alt="Logo" /></p></li>
           <li><a href="#" className="hover:underline"><p>Auctions</p></a></li>
           <li><a href="#" className="hover:underline"><p>Community</p></a></li>
         </ul>
 
         {/*//? searchinput */}
         <div className="flex items-center gap-4" id="search">
-            <div className="relative w-full md:w-40 " >
+            <div className="relative w-full md:w-40 select-none" >
               <input
                 type="text"
                 placeholder="Search..."
                 className="border rounded-full w-full px-4 py-2 text-sm w-full focus:outline-none focus:ring-2 focus:ring-[#C40D2E] pr-10"
               />
-              <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#C40D2E] pointer-events-none">
-                <FontAwesomeIcon icon={faSearch} />
+              <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#C40D2E] cursor-pointer ">
+                <FontAwesomeIcon icon={faSearch}  />
               </span>
             </div>
-            <button className="text-[#C40D2E]"><FontAwesomeIcon icon={faShoppingCart} /></button>
-            <button className="text-[#C40D2E]"><FontAwesomeIcon icon={faUser} /></button>
+            <button className="text-[#C40D2E] cursor-pointer"><FontAwesomeIcon icon={faShoppingCart} /></button>
+            <button className="text-[#C40D2E] cursor-pointer"><FontAwesomeIcon icon={faUser} /></button>
         </div>
       </div>
 
@@ -74,7 +75,7 @@ function Header(props) {
             animate={{ x: 0 }}
             exit={{ x: "-100%" }}
             transition={{ duration: 0.3 }}
-            className="w-full p-6 sm:w-60 dark:bg-gray-50 dark:text-gray-800 absolute z-50 top-15"
+            className="w-full  p-6 sm:w-60 dark:bg-gray-50 dark:text-gray-800 fixed z-50 top-15"
           >
             <nav className="space-y-8 text-sm">
               <div className="space-y-2">
