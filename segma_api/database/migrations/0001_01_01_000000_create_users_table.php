6949675a45b1c39ab72931d7c8eba5eb118938ed
abd ellah ikkou <mouniraz772@gmail.com>
2025-04-05 23:00:57 +0000

    switch type_db to mysql

--- segma_api/database/migrations/0001_01_01_000000_create_users_table.php
+++ segma_api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use MongoDB\Laravel\Schema\Blueprint;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 
@@ -12,28 +12,29 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $collection) {
-            $collection->index('name');
-            $collection->unique('email');
-            $collection->index('email_verified_at');
-            $collection->index('password');
-            $collection->rememberToken();
-            $collection->timestamps();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $collection) {
-            $collection->unique('email');
-            $collection->index('token');
-            $collection->index('created_at');
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         }); 
-// still in workig...
-        Schema::create('sessions', function (Blueprint $collection) {
-            $collection->string('id')->primary();
-            $collection->foreignId('user_id')->nullable()->index();
-            $collection->string('ip_address', 45)->nullable();
-            $collection->text('user_agent')->nullable();
-            $collection->longText('payload');
-            $collection->integer('last_activity')->index();
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
--- segma_front/src/components/Layout.jsx
+++ segma_front/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 function Layout({ children }) {
   return (
-    <div className="grid grid-cols-1 grid-rows-[auto_1fr_auto]">
+    <div className="grid grid-cols-1 grid-rows-[auto_1fr_auto] font-(family-name:--site-font)">
       <Header />
       <main className="overflow-auto">{children}</main>
       <Footer />
