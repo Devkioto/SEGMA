d943d2346f32f99e11e695fb16f0a0408ee49ebb
abd ellah ikkou <mouniraz772@gmail.com>
2025-04-10 17:28:25 +0100

    abd ellah: ajouter stream page

--- segma_front/src/App.jsx
+++ segma_front/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Home from "./pages/Home";
 import Community from "./pages/Community";
 import Auctions from "./pages/Auctions";
+import Stream from "./pages/Stream";
 
 function App() {
   // UseSmoothScroll();
   return (
-    <Auctions/>
+    <Stream/>
+    // <Auctions/>
     // <Community/>
     // <Home/>
   );
